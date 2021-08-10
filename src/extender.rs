a51bbe1343ede67f6a6bf2af9ed68a039348e3b9
Paul Metcalfe <paul.metcalfe@gmail.com>
2021-08-10 19:32:44 +0100

    Add in FusedFutre impl where appropriate

--- src/extender.rs
+++ src/extender.rs
@@ -1,4 +1,4 @@
-use futures::{Future, Stream};
+use futures::{future::FusedFuture, ready, stream::FusedStream, Future, Stream};
 use pin_project_lite::pin_project;
 
 use core::pin::Pin;
@@ -6,37 +6,36 @@ use core::task::{Context, Poll};
 
 pin_project! {
     #[must_use = "futures do nothing unless you `.await` or poll them"]
-    pub struct StreamExtender<'c, A, B: ?Sized> {
+    pub struct StreamExtender<'dst, Dst: ?Sized, St> {
         #[pin]
-        stream: A,
-        extendable: &'c mut B
+        stream: St,
+        extendable: &'dst mut Dst
     }
 }
 
-impl<'c, A: Stream, B> Future for StreamExtender<'c, A, B>
+impl<'dst, Dst, St> Future for StreamExtender<'dst, Dst, St>
 where
-    A: Stream,
-    <A as Stream>::Item: IntoIterator,
-    B: Extend<<<A as Stream>::Item as IntoIterator>::Item>,
+    St: Stream,
+    <St as Stream>::Item: IntoIterator,
+    Dst: Extend<<<St as Stream>::Item as IntoIterator>::Item>,
 {
     type Output = ();
 
     fn poll(mut self: Pin<&mut Self>, context: &mut Context<'_>) -> Poll<Self::Output> {
         loop {
             let this = self.as_mut().project();
-            match this.stream.poll_next(context) {
-                Poll::Ready(Some(item)) => {
+            match ready!(this.stream.poll_next(context)) {
+                Some(item) => {
                     this.extendable.extend(item);
                 }
-                Poll::Ready(None) => break Poll::Ready(()),
-                Poll::Pending => break Poll::Pending,
+                None => break Poll::Ready(()),
             }
         }
     }
 }
 
 pub trait StreamExtendable<A>: Extend<A> {
-    fn stream_extend<St>(&mut self, stream: St) -> StreamExtender<'_, St, Self> {
+    fn stream_extend<St>(&mut self, stream: St) -> StreamExtender<'_, Self, St> {
         StreamExtender {
             stream,
             extendable: self,
@@ -45,3 +44,13 @@ pub trait StreamExtendable<A>: Extend<A> {
 }
 
 impl<T, A> StreamExtendable<A> for T where T: Extend<A> {}
+
+impl<'dst, Dst, St> FusedFuture for StreamExtender<'dst, Dst, St>
+where
+    StreamExtender<'dst, Dst, St>: Future,
+    St: FusedStream,
+{
+    fn is_terminated(&self) -> bool {
+        self.stream.is_terminated()
+    }
+}
